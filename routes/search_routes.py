2abe22c8605065a48c5f78a254c16f266477524f
LorenzoOsorioGS <osoriolorenzo@hotmail.com>
2026-04-10 18:35:39 -0300

    chore: remove backend files from repository tracking

--- routes/search_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from controllers.search_controller import handle_search
-
-search_bp = Blueprint('search_routes', __name__)
-
-# Rota POST para receber a busca
-search_bp.route('/search', methods=['POST'])(handle_search)
